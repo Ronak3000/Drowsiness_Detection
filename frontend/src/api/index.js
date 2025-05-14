02f3b21bdc5cf6710c3e6f3ade18c6499f1b6a02
Ronak3000 <ronakeshu3000@gmail.com>
2025-05-15 03:14:18 +0530

    second

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,4 +1,4 @@
-export const API_URL='https://<your-render-service>.onrender.com/predict'
+export const API_URL='https://drowsiness-detection-9l65.onrender.com'
 export async function analyzeFrame(data){
   const form=new FormData()
   form.append('frame',{uri:data, type:'image/jpeg', name:'frame.jpg'})
